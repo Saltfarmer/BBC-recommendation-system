92d19033a94dcd460b6f692a3ff4107255064d8f
Candra Gctk <candragctk17@gmail.com>
2022-03-10 16:25:38 +0100

    Add some buttons to app
    The button still not working though

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
 st.set_page_config(layout="wide")
+
 df = pd.read_csv('BBC_metadata.csv')
 
-st.title("BBC News recommendation system (content based)")
+st.title("BBC recommendation system (content based)")
 
 # create a cover and info column to display the selected book
 cover, info = st.columns([2, 3])
 
-# Showing random news
 rand = random.randint(0, len(df))
 
 with cover:
@@ -24,9 +24,11 @@ with cover:
 with info:
   # display the book information
   st.title(df['title'].iloc[rand])
-  st.markdown(df['description'].iloc[rand])
-  st.markdown(df['url'].iloc[rand])
-  st.markdown(df['keywords'].iloc[rand])  
+  st.header("Description")
+  st.markdown(df['description'].iloc[rand])  
+  st.markdown("Keywords : " + df['keywords'].iloc[rand])
+  if st.button("Watch"):
+    st.markdown(df['url'].iloc[rand])
 
 # Clustering
 cluster = st.slider("Choose number of clusters", 2, 10)
@@ -46,12 +48,18 @@ df['cluster'] = kmeans.labels_
 
 st.dataframe(df.head(10))
 
+# Let user choose the category
+choice = st.selectbox("Choose the category", df['category'].unique().tolist())
+
 # Showing recommendation according to random same cluster
+sample = df[(df['cluster'] == df['cluster'].iloc[rand]) & (df['category'] == choice)].sample(n=5)
+
 columns = st.columns(5)
 
 for i in range(5):
     with columns[i]:
-        st.header(df['title'].iloc[rand])
-        st.image(df['image'].iloc[rand])
+        if st.button(sample['title'].iloc[i]):
+          st.session_state['idx'] = df[df['title'] == sample['title'].iloc[i]].index.tolist()[0]
+        st.image(sample['image'].iloc[i])
         
 
