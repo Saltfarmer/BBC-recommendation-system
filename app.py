27cb0e5ac62f25b8fddfd6baece59756b5821613
Candra Gctk <candragctk17@gmail.com>
2022-03-12 22:57:05 +0100

    Working button
    Working button with args and session_state

--- app.py
+++ app.py
@@ -12,29 +12,30 @@ df = pd.read_csv('BBC_metadata.csv')
 
 st.title("BBC recommendation system (content based)")
 st.title("")
-st.image("https://ichef.bbci.co.uk/images/ic/1920x1080/p09xtmrp.jpg")
+# st.image("https://ichef.bbci.co.uk/images/ic/1920x1080/p09xtmrp.jpg")
 
 # create a cover and info column to display the selected book
 cover, info = st.columns([2, 3])
 
-rand = random.randint(0, len(df))
+if 'key' not in st.session_state:
+  st.session_state['key'] = random.randint(0, len(df))
+
+key = st.session_state['key']
 
 with cover:
   # display the image
-  st.image(df['image'].iloc[rand])
+  st.image(df['image'].iloc[key])
 
 with info:
   # display the book information
-  st.header(df['title'].iloc[rand])
-  st.markdown(df['description'].iloc[rand])  
-  st.markdown("Keywords : " + df['keywords'].iloc[rand])
-  if st.button("Watch"):
-    st.markdown(df['url'].iloc[rand])
+  st.header(df['title'].iloc[key])
+  st.markdown(df['description'].iloc[key])  
+  st.caption("Keywords : " + df['keywords'].iloc[key])
+  st.markdown("# [Watch It](" + df['url'].iloc[key] + ")", unsafe_allow_html=True)
 
 # Clustering
-st.title("What is K-means")
-st.markdown("k-means clustering is a method of vector quantization, originally from signal processing, that aims to partition n observations into k clusters in which each observation belongs to the cluster with the nearest mean (cluster centers or cluster centroid), serving as a prototype of the cluster. This results in a partitioning of the data space into Voronoi cells. k-means clustering minimizes within-cluster variances (squared Euclidean distances), but not regular Euclidean distances, which would be the more difficult Weber problem: the mean optimizes squared errors, whereas only the geometric median minimizes Euclidean distances. For instance, better Euclidean solutions can be found using k-medians and k-medoids. ")
-
+st.title("What is K-means ?")
+st.markdown("The idea of k-means is that we assume there are k groups in our dataset. We then try to group the data into those k groups. Each group is described by a single point known as a centroid. The centroid of a cluster is the mean value of the points within the cluster. So as a user, you are free to choose the number of `k` cluster")
 cluster = st.slider("Choose number of clusters", 2, 10)
 
 list_description = df['description'].tolist()
@@ -56,14 +57,16 @@ df['cluster'] = kmeans.labels_
 choice = st.selectbox("Choose the category", df['category'].unique().tolist())
 
 # Showing recommendation according to random same cluster
-sample = df[(df['cluster'] == df['cluster'].iloc[rand]) & (df['category'] == choice)].sample(n=5)
+sample = df[(df['cluster'] == df['cluster'].iloc[key]) & (df['category'] == choice)].sample(n=5)
 
 columns = st.columns(5)
 
+def keychanger(keymaker):
+  st.session_state['key'] = keymaker
+
 for i in range(5):
     with columns[i]:
-        if st.button(sample['title'].iloc[i]):
-          st.session_state['idx'] = df[df['title'] == sample['title'].iloc[i]].index.tolist()[0]
-        st.image(sample['image'].iloc[i])
-        
-
+      keymaker = df[df['title'] == sample['title'].iloc[i]].index.tolist()[0]
+      st.markdown(keymaker) 
+      st.button(sample['title'].iloc[i], on_click=keychanger, args=(keymaker, ))
+      st.image(sample['image'].iloc[i])
